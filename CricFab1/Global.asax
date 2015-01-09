7390f54dd3f6c60371bfd8d97345a5d165c7ebf2
kasyap5 <kasyap5@gmail.com>
2015-01-10 00:30:04 +0530

    Initial Commit

---
+++ CricFab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CricFab1.MvcApplication" Language="C#" %>
---
+++ CricFab1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CricFab1.Startup))]
+namespace CricFab1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
